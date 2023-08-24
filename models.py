c7040622c409943bf54a7b4e94732f0efea99dac
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-08-23 18:42:20 -0700

    adds fucntionality

--- models.py
+++ models.py
@@ -101,6 +101,82 @@ class UserApp:
         print(f"Welcome, {user.first_name}!")
         return user
 
+    def account_settings(self):
+        if not self.logged_in_user:
+            print("You need to log in first.")
+            return
+
+        print("Account Settings:")
+        print("1. Edit User Account\n2. Delete User Account\n3. Back to Main Menu")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            self.edit_user_account()
+        elif choice == "2":
+            self.delete_user_account()
+
+    def edit_user_account(self):
+        if not self.logged_in_user:
+            print("You need to log in first.")
+            return
+
+        new_first_name = input("Enter new first name: ")
+        new_last_name = input("Enter new last name: ")
+        new_username = input("Enter new username: ")
+        new_password = input("Enter new password: ")
+
+        self.logged_in_user.first_name = new_first_name
+        self.logged_in_user.last_name = new_last_name
+        self.logged_in_user.username = new_username
+        self.logged_in_user.set_password(new_password)
+
+        self.session.commit()
+        print("User account updated successfully!")
+
+    def delete_user_account(self):
+        if not self.logged_in_user:
+            print("You need to log in first.")
+            return
+
+        confirmation = input("Are you sure you want to delete your account? (yes/no): ").lower()
+        if confirmation == "yes":
+            self.session.delete(self.logged_in_user)
+            self.session.commit()
+            self.logged_in_user = None
+            print("User account deleted successfully!")
+        else:
+            print("Account deletion cancelled.")
+
+    def your_passwords(self):
+        if not self.logged_in_user:
+            print("You need to log in first.")
+            return
+
+        for _ in range(3):
+            question = random.choice(self.logged_in_user.security_questions)
+            user_answer = input(f"Answer the following security question: {question.question}\n")
+
+            if user_answer == question.answer:
+                break
+            else:
+                print("Security question answer is incorrect.")
+        else:
+            print("Failed to answer security questions. Access denied.")
+            return
+
+        print("Your Passwords:")
+        print("1. Add Password\n2. Get Passwords\n3. Edit Password\n4. Delete Password\n5. Back to Main Menu")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            self.add_password()
+        elif choice == "2":
+            self.get_password()
+        elif choice == "3":
+            self.edit_password()
+        elif choice == "4":
+            self.delete_password()
+    
     def add_password(self):
         if not self.logged_in_user:
             print("You need to log in first.")
@@ -121,19 +197,65 @@ class UserApp:
             print("You need to log in first.")
             return
 
-        random_security_question = random.choice(self.logged_in_user.security_questions)
-        user_answer = input(f"Answer the following security question: {random_security_question.question}\n")
-        
-        if user_answer == random_security_question.answer:
-            passwords = self.session.query(Password).filter_by(user_id=self.logged_in_user.id).all()
-            if not passwords:
-                print("No passwords stored.")
-            else:
-                print("Stored passwords:")
-                for password in passwords:
-                    print(f"Website: {password.website}, Username: {password.username}, Password: {password.password}, Date Added: {password.date_added}")
+        passwords = self.session.query(Password).filter_by(user_id=self.logged_in_user.id).all()
+        if not passwords:
+            print("No passwords stored.")
         else:
-            print("Security question answer is incorrect.")
+            print("Stored passwords:")
+            for password in passwords:
+                print(f"Website: {password.website}, Username: {password.username}, Password: {password.password}, Date Added: {password.date_added}")
+    
+    def edit_password(self):
+        if not self.logged_in_user:
+            print("You need to log in first.")
+            return
+
+        passwords = self.session.query(Password).filter_by(user_id=self.logged_in_user.id).all()
+        if not passwords:
+            print("No passwords stored.")
+            return
+
+        print("Stored passwords:")
+        for idx, password in enumerate(passwords, start=1):
+            print(f"{idx}. Website: {password.website}, Username: {password.username}")
+
+        password_choice = int(input("Select a password to edit (enter number): "))
+        selected_password = passwords[password_choice - 1]
+
+        new_website = input("Enter the new website: ")
+        new_username = input("Enter the new username: ")
+        new_password = input("Enter the new password: ")
+        new_notes = input("Enter new notes (optional): ")
+
+        selected_password.website = new_website
+        selected_password.username = new_username
+        selected_password.password = new_password
+        selected_password.notes = new_notes
+
+        self.session.commit()
+        print("Password updated successfully!")
+
+    def delete_password(self):
+        if not self.logged_in_user:
+            print("You need to log in first.")
+            return
+
+        passwords = self.session.query(Password).filter_by(user_id=self.logged_in_user.id).all()
+        if not passwords:
+            print("No passwords stored.")
+            return
+
+        print("Stored passwords:")
+        for idx, password in enumerate(passwords, start=1):
+            print(f"{idx}. Website: {password.website}, Username: {password.username}")
+
+        password_choice = int(input("Select a password to delete (enter number): "))
+        selected_password = passwords[password_choice - 1]
+
+        self.session.delete(selected_password)
+        self.session.commit()
+        print("Password deleted successfully!")
+
 
     def run_app(self):
         Base.metadata.create_all(bind=self.engine)  # Create user tables if they don't exist
@@ -151,13 +273,13 @@ class UserApp:
                 else:
                     print("Invalid choice. Please select a valid option.")
             else:
-                print("1. Add Password\n2. Get Password\n3. Logout")
+                print("1. Account Settings\n2. Your Passwords\n3. Logout")
                 choice = input("Enter your choice: ")
                 
                 if choice == "1":
-                    self.add_password()
+                    self.account_settings()
                 elif choice == "2":
-                    self.get_password()
+                    self.your_passwords()
                 elif choice == "3":
                     self.logged_in_user = None
                 else:
